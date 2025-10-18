6a2f9e76529a58c185f32bc50565f2ab7f96f211
ems480 <116377770+ems480@users.noreply.github.com>
2025-10-18 05:23:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -999,6 +999,7 @@ def initiate_payment():
         data = request.json
         phone = data.get("phone")
         amount = data.get("amount")
+        ccorrespondent - data.get("correspondent")
         if not phone or not amount:
             return jsonify({"error": "Missing phone or amount"}), 400
 
@@ -1009,7 +1010,7 @@ def initiate_payment():
             "depositId": deposit_id,
             "amount": str(amount),
             "currency": "ZMW",
-            "correspondent": "MTN_MOMO_ZMB",
+            "correspondent": correspondent #"MTN_MOMO_ZMB",
             "payer": {"type": "MSISDN", "address": {"value": phone}},
             "customerTimestamp": customer_ts,
             "statementDescription": "StudyCraftPay",
@@ -6123,6 +6124,7 @@ def get_pending_loans():
 # # #         init_db()
 # # #     port = int(os.environ.get("PORT", 5000))
 # # #     app.run(host="0.0.0.0", port=port)
+
 
 
 
